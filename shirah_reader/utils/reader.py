76697de668507b1edceb9b299345243a49fdbea0
Vishwas Mittal <vishwas.mittal78@gmail.com>
2021-05-15 13:06:18 +0530

    Add unimplemented utility functions to handle user inputs on ebook reading screen

---
+++ shirah_reader/utils/reader.py
@@ -0,0 +1,97 @@
+# handle actions
+
+
+def handle_scroll_up():
+    pass
+
+
+def handle_scroll_down():
+    pass
+
+
+def handle_page_up():
+    pass
+
+
+def handle_page_down():
+    pass
+
+
+def handle_half_screen_adjustment():
+    pass
+
+
+def handle_jump_to_next_chapter():
+    pass
+
+
+def handle_jump_to_prev_chapter():
+    pass
+
+
+def handle_jump_to_beginning_of_chapter():
+    pass
+
+
+def handle_jump_to_end_of_chapter():
+    pass
+
+
+def handle_show_table_of_contents():
+    pass
+
+
+def handle_show_metadata():
+    pass
+
+
+def handle_show_help():
+    pass
+
+
+def handle_screen_size_enlargement():
+    pass
+
+
+def handle_screen_size_decrement():
+    pass
+
+
+def handle_screen_width_adjustment():
+    pass
+
+
+def handle_regex_search():
+    pass
+
+
+def handle_image_display():
+    pass
+
+
+def handle_color_switch():
+    pass
+
+
+def handle_add_bookmark():
+    pass
+
+
+def handle_display_bookmarks():
+    pass
+
+
+def handle_word_definition():
+    pass
+
+
+def handle_marking_position():
+    pass
+
+
+def handle_jump_to_position():
+    pass
+
+
+def handle_toggle_progress_display():
+    pass
